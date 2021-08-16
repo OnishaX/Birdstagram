c60dbd484f115f1db96eb29d284810c5ec88c3a9
Onisha Juhl <onishaj@icloud.com>
2021-08-17 11:40:02 +1200

    began implementing the user on posts and changed the sizing a bit

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,8 @@ const feedArray = [
     "postTitle": "A Kiwi",
     "postDescription": "this is a post about a kiwi",
     "bird": "Kiwi",
-    "userID":"69",
+    "userName": "Adam lambert",
+    "userID": "username69",
     "comments": [{
       "commentID": "420",
       "commentDateD": "07",
@@ -37,81 +38,95 @@ const feedArray = [
             "userID": "420"
           }]
         }]
-  }
-    ]}]},
-    {
-      "postID": "1",
-      "postTitle": "A Kiwi",
-      "postDescription": "this is a post about a kiwi",
-      "bird": "Kiwi",
-      "userID":"69",
-      "comments": [{
-        "commentID": "420",
-        "commentDateD": "07",
-        "commentDateM": "11",
-        "commentDateY": "2020",
-        "commentTimeH": "6",
-        "commentTimeM": "9",
-        "commentTimeS": "0",
+      }
+      ]
+    }]
+  },
+  {
+    "postID": "1",
+    "postTitle": "A Kiwi",
+    "postDescription": "this is a post about a kiwi",
+    "bird": "Kiwi",
+    "userName": "Adam lambert",
+    "userID": "username69",
+    "comments": [{
+      "commentID": "420",
+      "commentDateD": "07",
+      "commentDateM": "11",
+      "commentDateY": "2020",
+      "commentTimeH": "6",
+      "commentTimeM": "9",
+      "commentTimeS": "0",
+      "userID": "420",
+      "commentContents": "WOOOO",
+
+      "commentLikes": [{
+        "date": "07.11.2020",
+        "time": "13.04.09",
         "userID": "420",
-        "commentContents": "WOOOO",
-  
-        "commentLikes": [{
-          "date": "07.11.2020",
-          "time": "13.04.09",
+
+        "replies": [{
+          "replyID": "420",
+          "replyDateD": "07",
+          "replyDateM": "11",
+          "replyDateY": "2020",
+          "replyTimeH": "6",
+          "replyTimeM": "9",
+          "replyTimeS": "0",
           "userID": "420",
-  
-          "replies": [{
-            "replyID": "420",
-            "replyDateD": "07",
-            "replyDateM": "11",
-            "replyDateY": "2020",
-            "replyTimeH": "6",
-            "replyTimeM": "9",
-            "replyTimeS": "0",
-            "userID": "420",
-            "replyContents": "WOOOO",
-            "replyLikes": [{
-              "date": "07.11.2020",
-              "time": "13.04.09",
-              "userID": "420"
-            }]
+          "replyContents": "WOOOO",
+          "replyLikes": [{
+            "date": "07.11.2020",
+            "time": "13.04.09",
+            "userID": "420"
           }]
-    }
-      ]}]}
-  
-  ]
+        }]
+      }
+      ]
+    }]
+  }
+
+]
 
 function init() {
   displayFeed(feedArray);
   console.log("Init Triggered")
-  }
+}
 
 function displayFeed(feedArray) {
-    console.log("displayFeed Triggered")
-    let html = "";
-    for (const post of feedArray) {
-      html += `<div class="post" data-id="${post.id}" style="background-image: url(../images/${post.postID}.png);">
+  console.log("displayFeed Triggered")
+  let html = "";
+  for (const post of feedArray) {
+    html += `<div class="post" data-id="${post.id}" style="background-image: url(../images/${post.postID}.png);">
 				<div class="image" style="background-image: url(../images/${post.postID}.png);">
 				</div>
 				<div class="meta">
 					<div class="metaPost">
-						<div class="metaPostLeft">
+						<div class="metaLeft">
 							<div class="title">${post.postTitle}</div>
 							<div class="subtitle">${post.postDescription}</div>
 						</div>
-						<div class="metaPostRight">
-            <div class="subtitle">${post.bird}</div>
-						</div>
+              <div class="metaRight">
+                <div class="subtitle">${post.bird}</div>
+              </div>
+            </div>
+            <hr>
+            <div class="metaPost">
+              <div class="metaLeft">
+						    <div class="title2">${post.userName}</div>
+							  <div class="subtitle">@${post.userID}</div>
+              </div>
+              <div class="metaRight">
+              </div>
+            </div>
 					</div>
-					<hr>
 				</div>
 			</div>`;
-    }
-    console.log("html appended")
-    $("#content").html(html)
-
   }
+  console.log("html appended")
+  $("#content").html(html)
+
+}
 
 init();
 console.log("Init Trigger")
